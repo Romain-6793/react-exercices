8375619a631b1b456affdfc84fbe85b97eac1bbe
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2022-10-19 10:41:21 +0200

    main layout set

--- react-exercices/src/App.js
+++ react-exercices/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TaskListBox from './components/TaskListBox';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="big-title">Crée ta liste de tâches</h1>
+      <TaskListBox />
     </div>
   );
 }
---
+++ react-exercices/src/components/TaskListBox.js
@@ -0,0 +1,15 @@
+
+import "../styles/TaskListBox.css"
+
+function TaskListBox() {
+    return (
+        <div className="container-flex">
+            <div className="big-container">
+                <button className="add-button"></button>
+                <div className="list-space"></div>
+            </div>
+        </div>
+    )
+}
+
+export default TaskListBox
